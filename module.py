3527279599d969c0f8a97ebdde9740bcd1ab4415
Shubham Jagannath Pathare <55013966+shubhampathare777@users.noreply.github.com>
2024-04-10 19:38:05 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def add(a,b):
+    return a+b
+def sub(c,d):
+    return c-d
+def mul(e,f):
+    return e*f
+def div(g,h):
+    returng/h
+
+
+# In[ ]:
+
+
+
+
